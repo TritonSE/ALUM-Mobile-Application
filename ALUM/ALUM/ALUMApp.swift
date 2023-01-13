342c95028a2c5e30e2c3e81b115440738af31e63
Aman Aggarwal <akaggarw@ucsd.edu>
2023-01-12 22:06:22 -0800

    Initialize frontend

---
+++ ALUM/ALUM/ALUMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALUMApp.swift
+//  ALUM
+//
+//  Created by Aman Aggarwal on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ALUMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
