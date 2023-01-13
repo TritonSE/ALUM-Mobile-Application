f3605be3cf747ec0f2dc87fafb31b470788f8015
Aman Aggarwal <akaggarw@ucsd.edu>
2023-01-12 22:06:22 -0800

    Initialize frontend

---
+++ ALUM/ALUM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ALUM
+//
+//  Created by Aman Aggarwal on 1/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
