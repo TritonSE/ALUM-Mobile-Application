286bc9e2f3b867001e5a76eb515475c18d4b5504
HarshGurnani <gurnani.harsh@gmail.com>
2023-01-20 11:21:10 -0800

    Put Password and TextField components together, also added a labelText field to specify what the user should input

--- ALUM/ALUM/Components/TextInputFieldComponent.swift
+++ ALUM/ALUM/Components/TextInputFieldComponent.swift
@@ -9,12 +9,20 @@ import SwiftUI
 
 struct TextInputFieldComponent: View {
     @State var textFieldText: String = ""
+    @State var isSecured: Bool = false
+    @State var showEye: Bool = false
+    var labelText: String = ""
         
     var body: some View {
         
-        VStack {
-            TextField("Type something here...",
-                      text: $textFieldText)
+        ZStack (alignment: .trailing){
+            Group {
+                if isSecured {
+                    SecureField(labelText, text: $textFieldText)
+                } else {
+                    TextField(labelText, text: $textFieldText)
+                }
+            }
             .padding(16.0)
             .frame(height: 48.0)
             .background(
@@ -25,6 +33,16 @@ struct TextInputFieldComponent: View {
                 RoundedRectangle(cornerRadius: 8.0).stroke(Color("NeutralGray3"), lineWidth: 1.0)
             )
             
+            if showEye {
+                Button(action: {
+                    isSecured.toggle()
+                }) {
+                    Image(systemName: isSecured ? "eye.slash" : "eye")
+                        .accentColor(Color("NeutralGray4"))
+                }
+                .padding(14.0)
+            }
+            
         }
         .padding()
     }
@@ -32,6 +50,6 @@ struct TextInputFieldComponent: View {
 
 struct TextInputFieldView_Previews: PreviewProvider {
     static var previews: some View {
-        TextInputFieldComponent()
+        TextInputFieldComponent(isSecured: false, showEye: false, labelText: "Username:")
     }
 }
