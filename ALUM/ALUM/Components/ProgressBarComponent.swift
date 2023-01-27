95324f0703b0eacf8083e229df733bdd8d1a7bf2
Yashwanth Ravipati <yashravipatischool@gmail.com>
2023-01-27 15:49:13 -0800

    Changing to include state vars for filled nodes, active node, and total nodes

--- ALUM/ALUM/Components/ProgressBarComponent.swift
+++ ALUM/ALUM/Components/ProgressBarComponent.swift
@@ -10,26 +10,49 @@
 import SwiftUI
 
 struct ProgressBarComponent: View{
-    @State var filledNodes = 4
+    @State var Nodes = 6
+    @State var filledNodes = 2
+    @State var activeNode = 3
     
     
     
     var body: some View {
         ZStack{
             HStack{
-                ForEach(0 ..< filledNodes - 1, id:\.self){ _ in
-                    Rectangle()
-                        .frame(width: 375/(Double(filledNodes) - 1) - 5, height: 2)
-                        .foregroundColor(Color("ALUM Dark Blue"))
+                ForEach(0 ..< Nodes - 1, id:\.self){ index in
+                    if(index < activeNode-1 || index < filledNodes - 1){
+                        Rectangle()
+                            .frame(width: 350/(Double(Nodes) - 1) - 5, height: 2)
+                            .foregroundColor(Color("ALUM Dark Blue"))
+                    }
+                    else{
+                        Rectangle()
+                            .frame(width: 350/(Double(Nodes) - 1) - 5, height: 2)
+                            .foregroundColor(Color("NeutralGray2"))
+                    }
                 }
             }
             HStack{
-                ForEach(0 ..< filledNodes, id:\.self){ _ in
-                    Circle()
-                        .strokeBorder(Color("ALUM Dark Blue"),lineWidth: 3)
+                ForEach(0 ..< Nodes, id:\.self){ index in
+                    if(index == activeNode - 1){
+                        Circle()
+                            .strokeBorder(Color("ALUM Dark Blue"),lineWidth: 3)
                             .background(Circle().foregroundColor(Color("ALUM Light Blue")))
-                        .frame(width: 16, height: 16)
-                        .padding(.horizontal, 175 / (Double(filledNodes - 1)) - (5 + Double(filledNodes)))
+                            .frame(width: 16, height: 16)
+                            .padding(.horizontal, 170 / (Double(Nodes - 1)) - (5 + Double(Nodes)))
+                    }
+                    else if (index < filledNodes){
+                        Circle()
+                            .foregroundColor(Color("ALUM Dark Blue"))
+                            .frame(width: 16, height: 16)
+                            .padding(.horizontal, 170 / (Double(Nodes - 1)) - (5 + Double(Nodes)))
+                    }
+                    else{
+                        Circle()
+                            .foregroundColor(Color("NeutralGray2"))
+                            .frame(width: 16, height: 16)
+                            .padding(.horizontal, 170 / (Double(Nodes - 1)) - (5 + Double(Nodes)))
+                    }
                 }
             }
         }
