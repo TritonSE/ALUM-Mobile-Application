82ac13543304e6d0c6f2ad3e09785cc25be78878
Sidhant Rohatgi <rohatgisidhant@gmail.com>
2023-02-08 19:46:42 -0800

    Made white colored tag a button

--- ALUM/ALUM/Components/SearchableTags.swift
+++ ALUM/ALUM/Components/SearchableTags.swift
@@ -14,45 +14,74 @@ struct SearchableTags: View {
     @State var isShowing: Bool = true
 
     var body: some View {
-        VStack {
-            ZStack {
-                HStack {
+        if isBlue {
+            VStack {
+                ZStack {
+                    HStack {
 
-                    Text(text)
-                        .foregroundColor(Color(.black))
-                    Spacer()
-                        .frame(width: 10)
+                        Text(text)
+                            .foregroundColor(Color(.black))
+                        Spacer()
+                            .frame(width: 10)
 
-                    Button(action: {
-                        isShowing = false
-                    }, label: {
-                        Image(systemName: "xmark")
-                    })
-                    .foregroundColor(Color("NeutralGray4"))
-                    .frame(height: 8)
-                    .frame(width: 8)
-//                    Image(systemName: "xmark")
-//                        .foregroundColor(Color("NeutralGray4"))
-//                        .frame(height: 8)
-//                        .frame(width: 8)
+                        Button(action: {
+                            isShowing = true
+                        }, label: {
+                            Image(systemName: "xmark")
+                        })
+                        .foregroundColor(Color("NeutralGray4"))
+                        .frame(height: 8)
+                        .frame(width: 8)
+
+                    }
 
                 }
+                .padding(10)
+                .background(RoundedRectangle(cornerRadius: 100).stroke())
+                .foregroundColor(Color("Alum Light Blue"))
+                .font(.headline)
+                .background(Color("Alum Light Blue"))
+                .cornerRadius(100)
 
-            }
-            .padding(10)
-            .background(RoundedRectangle(cornerRadius: 100).stroke())
-            .foregroundColor(Color("Alum Light Blue"))
-            .font(.headline)
-            .background(Color("Alum Light Blue"))
-            .cornerRadius(100)
+                }
+        } else {
+            VStack {
+                ZStack {
+                    HStack {
+                        Button(action: {isShowing = true}, label: {
+                            Spacer()
+                                .frame(width: 18)
 
-            }
+                            Image(systemName: "plus")
+                                .foregroundColor(Color("ALUM Dark Blue"))
+
+                            Spacer()
+                                .frame(width: 14)
+
+                            Text(text)
+                                .foregroundColor(Color("ALUM Dark Blue"))
+                            Spacer()
+                                .frame(width: 12)
+                        })
+                    }
+
+                }
+                .padding(10)
+                .background(RoundedRectangle(cornerRadius: 100).stroke())
+                .foregroundColor(Color("Alum Light Blue"))
+                .font(.headline)
+                .background(Color(.white))
+                .cornerRadius(100)
+
+                }
+        }
+
     }
 }
 
 struct SearchableTags_Previews: PreviewProvider {
     static var previews: some View {
-        SearchableTags(text: "tag text", isBlue: true)
+        SearchableTags(text: "Sample Text @12345", isBlue: true)
 
     }
 }
