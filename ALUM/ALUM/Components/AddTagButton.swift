94bfc1a5cb5c5295aae894e544bb62aa74cc0d72
Sidhant Rohatgi <118028422+sidhantrohatgi@users.noreply.github.com>
2023-02-21 05:11:16 -0800

    Feature/JennySidhant/Tags (#42)

---
+++ ALUM/ALUM/Components/AddTagButton.swift
@@ -0,0 +1,52 @@
+//
+//  AddTagButton.swift
+//  ALUM
+//
+//  Created by Sidhant Rohatgi on 2/20/23.
+//
+
+import SwiftUI
+
+struct AddTagButton: View {
+
+    @State var text: String = ""
+    @State var isShowing: Bool = true
+
+    var body: some View {
+        VStack {
+            ZStack {
+                HStack {
+                    Button(action: {isShowing = true}, label: {
+                        Spacer()
+                            .frame(width: 18)
+
+                        Image(systemName: "plus")
+                            .foregroundColor(Color("ALUM Dark Blue"))
+
+                        Spacer()
+                            .frame(width: 14)
+
+                        Text(text)
+                            .foregroundColor(Color("ALUM Dark Blue"))
+                        Spacer()
+                            .frame(width: 12)
+                    })
+                }
+
+            }
+            .padding(10)
+            .background(RoundedRectangle(cornerRadius: 100).stroke())
+            .foregroundColor(Color("ALUM Light Blue"))
+            .font(.body)
+            .background(Color(.white))
+            .cornerRadius(100)
+
+            }
+    }
+}
+
+struct AddTagButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTagButton(text: "Sample Text 2 @12345")
+    }
+}
