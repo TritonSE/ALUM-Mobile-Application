e86ad73353955977de888e5c13e04380982bcc50
HarshGurnani <68934498+HarshGurnani@users.noreply.github.com>
2023-02-22 16:19:07 -0800

    Added sign up model

---
+++ ALUM/ALUM/Views/SignUpPage/SignUpModel.swift
@@ -0,0 +1,40 @@
+//
+//  SignUpModel.swift
+//  ALUM
+//
+//  Created by Yash Ravipati on 2/22/23.
+//
+
+import Foundation
+struct Account{
+    var name: String
+    var email: String
+    var password: String
+}
+struct Mentee {
+    var name: String
+    var email: String
+    var grade: String
+    var topicsOfInterest: [TagDisplay]
+    var careerInterests: [TagDisplay]
+    var mentorshipGoal: String
+    var password: String
+}
+struct Mentor {
+    var name: String
+    var email: String
+    var yearOfGrad : String
+    var university: String
+    var major: String
+    var minor: String
+    var intendedCareer: String
+    var password: String
+}
+
+
+
+
+
+
+
+
