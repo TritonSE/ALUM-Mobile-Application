87dad2c30a51ba4a0029556856be7c123482bf81
jennymar <jenmar528@gmail.com>
2023-02-28 23:18:21 -0800

    made DrawerContainer generic and ParagraphInput save data

---
+++ ALUM/ALUM/Components/DrawerContainer.swift
@@ -0,0 +1,93 @@
+//
+//  DrawerContainer.swift
+//  ALUM
+//
+//  Created by Jenny Mar on 2/17/23.
+//
+
+import SwiftUI
+
+struct DrawerContainer<Content: View>: View {
+    var cancelFunc: () -> Void
+    var doneFunc: (String) -> Void
+    @State var text: String = "hi"
+    @ViewBuilder var content: Content
+
+    var body: some View {
+        ZStack(alignment: .top) {
+            RoundedRectangle(cornerRadius: 16)
+                .frame(height: UIScreen.main.bounds.height * 0.9)
+                .foregroundColor(Color.white)
+            VStack {
+                HStack {
+                    Button("Cancel") {
+                        withAnimation(.spring()) {
+                            cancelFunc()
+                        }
+                    }
+                    .padding(.init(top: 26, leading: 0, bottom: 29, trailing: 16.0))
+                    .foregroundColor(Color("ALUM Dark Blue"))
+                    .font(.system(size: 13))
+                    Spacer()
+                    Button("Done") {
+                        withAnimation(.spring()) {
+                            doneFunc(text)
+                        }
+                    }
+                    .padding(.init(top: 26, leading: 0, bottom: 29, trailing: 0))
+                    .foregroundColor(Color("ALUM Dark Blue"))
+                    .font(.system(size: 13))
+                }
+                .frame(width: UIScreen.main.bounds.width * 0.9, alignment: .center)
+                content
+            }
+        }.transition(.move(edge: .bottom))
+    }
+}
+
+struct DrawerTester: View {
+    @State private var showingSheet = false
+    @State var data: String = "previous text"
+    @State var oldText: String = "previous text"
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("open drawer") {
+                    showingSheet.toggle()
+                }
+                .padding(120)
+                .frame(width: 358, height: 120)
+                .background(Color("ALUM White"))
+                .foregroundColor(Color.black)
+                .cornerRadius(12)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color("NeutralGray3"), lineWidth: 1))
+                .sheet(isPresented: $showingSheet) {
+                    DrawerContainer(cancelFunc: cancel, doneFunc: done) {
+                        ParagraphInput(question: "Why do you want to be a mentor?", text: $data)
+                    }
+                }
+
+                Text(data)
+            }
+        }
+    }
+
+    func done(textfield: String) {
+        oldText = data
+        showingSheet = false
+    }
+
+    func cancel() {
+        data = oldText
+        showingSheet = false
+    }
+}
+
+struct DrawerContainer_Previews: PreviewProvider {
+    static var previews: some View {
+        DrawerTester()
+    }
+}
---
+++ ALUM/ALUM/Components/ParagraphInput.swift
@@ -0,0 +1,69 @@
+//
+//  ParagraphInput.swift
+//  ALUM
+//
+//  Created by Jenny Mar on 2/15/23.
+//
+
+// call back function exists in parent class
+// when its called, it saves the text into the small paragraph
+// the method is called when done is pressed
+
+import SwiftUI
+
+struct ParagraphInput: View {
+    @State var question: String
+    @Binding var text: String
+    @State private var totalChars = 0
+    @State private var lastText = ""
+
+    var body: some View {
+        HStack {
+            Text(question)
+                .font(.system(size: 17))
+                .foregroundColor(Color("ALUM Dark Blue"))
+            Spacer()
+        }.padding(.init(top: 2.0, leading: 16.0, bottom: 2.0, trailing: 16.0))
+        TextEditor(text: $text)
+            .padding(10)
+            .frame(width: 358, height: 362)
+            .cornerRadius(12)
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color("NeutralGray3"), lineWidth: 1))
+            .colorMultiply(Color("ALUM White"))
+            .background(Color("ALUM White"))
+            .onChange(of: text, perform: { text in
+                totalChars = text.count
+                if totalChars <= 1000 {
+                        lastText = text
+                    } else {
+                        self.text = lastText
+                    }
+            })
+        HStack {
+            Spacer()
+            Text("\(totalChars) / 1000")
+                .font(.system(size: 13))
+                .foregroundColor(Color("NeutralGray3"))
+        }.padding(.init(top: 2.0, leading: 16.0, bottom: 2.0, trailing: 16.0))
+    }
+}
+
+struct ParagraphInputScreen: View {
+    @State var data: String = "add text here"
+    @State var textFieldText: String = ""
+
+    var body: some View {
+        VStack {
+            ParagraphInput(question: "Why do you want to be a mentor?", text: $textFieldText)
+            // Text(data)
+        }
+    }
+}
+
+struct ParagraphInput_Previews: PreviewProvider {
+    static var previews: some View {
+        ParagraphInputScreen()
+    }
+}
