c1ba83e0b3294ce4f7ce0088fa326b9ecaa4b520
christenx <99223047+christen03@users.noreply.github.com>
2023-03-12 17:59:36 -0700

    lint-fixed get route

--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -4,29 +4,20 @@ import { Note } from "../models/notes";
 const router = express.Router();
 
 router.get("/notes/:id", async (req: Request, res: Response, next: NextFunction) => {
+  try {
     console.log("Getting...");
     const id = req.params.id;
     const note = await Note.findById(id);
     if (note == null) {
-        console.log("notfound");
-        next();
-        return res.status(400);
+      throw new Error(); 
     }
-    return res.status(200).json(
-        note.answers
-    )
+    return res.status(200).json(note.answers);
+  } catch (e) {
+    next(e);
+    return res.status(400).json({
+      message: "Invalid ID!",
+    });
+  }
 });
-
-router.patch("/notes/:id", async(req: Request, res: Response, next: NextFunction) => {
-    console.log("Patching...");
-    const id = req.params.id;
-    const note=await Note.findById(id);
-    if(note == null){
-        console.log("not found");
-        next();
-        return res.status(400);
-    }
-    //edit the answers here!
-})
 
 export { router as notesRouter };
