26154df93e33dd394e31b67794f05a829a6351a2
christenx <99223047+christen03@users.noreply.github.com>
2023-03-12 18:08:07 -0700

    new get

--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -4,18 +4,21 @@ import { Note } from "../models/notes";
 const router = express.Router();
 
 router.get("/notes/:id", async (req: Request, res: Response, next: NextFunction) => {
-    console.log("Getting...");
-    const id = req.params.id;
-    const note = await Note.findById(id);
-    if (note == null) {
-        console.log("notfound");
-        next();
-        return res.status(400);
+    try {
+      console.log("Getting...");
+      const id = req.params.id;
+      const note = await Note.findById(id);
+      if (note == null) {
+        throw new Error(); 
+      }
+      return res.status(200).json(note.answers);
+    } catch (e) {
+      next(e);
+      return res.status(400).json({
+        message: "Invalid ID!",
+      });
     }
-    return res.status(200).json(
-        note.answers
-    )
-});
+  });
 
 router.patch("/notes/:id", async(req: Request, res: Response, next: NextFunction) => {
     console.log("Patching...");
