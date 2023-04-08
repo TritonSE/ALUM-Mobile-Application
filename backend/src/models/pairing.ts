bb4e92f5b65ae82efabdf85d97ec7874fc84f2e2
adhi0331 <adhi03.ananthan@gmail.com>
2023-04-08 12:42:30 -0700

    added pairing.ts

---
+++ backend/src/models/pairing.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+interface PairingInterface {
+    menteeId: string;
+    mentorId: string;
+    whyPaired: string;
+}
+
+interface PairingDoc extends mongoose.Document {
+    menteeId: string;
+    mentorId: string;
+    whyPaired: string; 
+}
+
+interface PairingModelInterface extends mongoose.Model<PairingDoc> {
+    build(atrr: PairingInterface): PairingDoc;
+}
+
+const pairingSchema = new mongoose.Schema({
+    menteeId: {
+        type: String,
+        required: true
+    },
+    mentorId: {
+        type: String,
+        required: true
+    },
+    whyPaired: {
+        type: String,
+        required: true
+    }
+});
+
+const Pairing = mongoose.model<PairingDoc, PairingModelInterface>("Pairing", pairingSchema);
+
+export { Pairing, pairingSchema }
