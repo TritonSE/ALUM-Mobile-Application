32f53d30f0562fa5dd45504e3fa63172d850fcca
adhi0331 <adhi03.ananthan@gmail.com>
2023-04-12 11:16:04 -0700

    added preliminary button

---
+++ ALUM/ALUM/ViewModels/CalendlyBookingViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CalendlyBookingViewModel.swift
+//  ALUM
+//  This file contains 
+//
+//  Created by Adhithya Ananthan on 4/12/23.
+//
+
+import Foundation
---
+++ ALUM/ALUM/Views/CalendlyBooking/CalendlyBooking.swift
@@ -0,0 +1,36 @@
+//
+//  CalendlyBooking.swift
+//  ALUM
+// This is a file used to store a dummy button for testing functionality of
+// booking an event via calendly. Note that the majority of the code in here will be transferred to
+// the profile view code once the PR has been updated
+//
+//  Created by Adhithya Ananthan on 4/12/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct CalendlyBooking: View  {
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Button {
+                
+            } label: {
+                Text("View My Calendly")
+                .font(Font.custom("Metropolis-Regular", size: 17, relativeTo: .headline))
+            }
+            .buttonStyle(FilledInButtonStyle())
+            .frame(width: 358)
+            .padding(.bottom, 26)
+        }
+    }
+}
+
+struct CalendlyBooking_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        CalendlyBooking()
+    }
+}
