2daabdfa143822ecea6b166bcc0954254cdb15e8
adhi0331 <adhi03.ananthan@gmail.com>
2023-04-13 10:51:05 -0700

    added calendly booking files

--- ALUM/ALUM/ViewModels/CalendlyBookingViewModel.swift
+++ ALUM/ALUM/ViewModels/CalendlyBookingViewModel.swift
@@ -1,9 +1,32 @@
 //
 //  CalendlyBookingViewModel.swift
 //  ALUM
-//  This file contains 
+//  This file contains helper functions to navigate the calendly
+// booking feature
 //
 //  Created by Adhithya Ananthan on 4/12/23.
 //
 
 import Foundation
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let calendlyLink = URL(string:"https://calendly.com/aananthanregina/30min")
+        let myRequest = URLRequest(url: calendlyLink!)
+        webView.load(myRequest)
+    }
+}
--- ALUM/ALUM/Views/CalendlyBooking/CalendlyBooking.swift
+++ ALUM/ALUM/Views/CalendlyBooking/CalendlyBooking.swift
@@ -11,6 +11,7 @@
 import Foundation
 import SwiftUI
 
+
 struct CalendlyBooking: View  {
     
     var body: some View {
