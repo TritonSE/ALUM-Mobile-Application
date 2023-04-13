59c4c76e4cec8c6653c621c3506677490fa63303
Yashwanth Ravipati <yashravipatischool@gmail.com>
2023-04-13 02:02:16 -0700

    Backend get functions

--- ALUM/ALUM/Services/UserService.swift
+++ ALUM/ALUM/Services/UserService.swift
@@ -31,24 +31,90 @@ struct MentorPostData: Codable {
     var mentorMotivation: String
 }
 
-struct MentorGetData: Codable {
+struct MentorGetData: Decodable {
+    var message: String
+    var mentor: MentorInfo
+    var whyPaired: String?
+}
+
+struct MentorInfo: Decodable {
     var menteeIDs: [String]?
     var id: String?
     var name: String
-    var about: String
     var imageId: String
+    var about: String
+    var calendlyLink: String
+    var graduationYear: Int
+    var college: String
     var major: String
     var minor: String
-    var college: String
     var career: String
-    var graduationYear: Int
-    var calendlyLink: String
     var topicsOfExpertise: [String]
     var mentorMotivation: String?
     var pairingIds: [String]?
     var organizationId: String?
     var personalAccessToken: String?
     var status: String?
+    var mongoVersion: Int?
+    var whyPaired: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case menteeIDs
+        case id = "_id"
+        case name
+        case imageId
+        case about
+        case calendlyLink
+        case graduationYear
+        case college
+        case major
+        case minor
+        case career
+        case topicsOfExpertise
+        case mentorMotivation
+        case pairingIds
+        case organizationId
+        case personalAccessToken
+        case status
+        case mongoVersion = "__v"
+        case whyPaired
+    }
+}
+
+struct MenteeGetData: Decodable {
+    var message: String
+    var mentee: MenteeInfo
+    var whyPaired: String?
+}
+
+struct MenteeInfo: Decodable {
+    var id: String?
+    var name: String
+    var imageId: String
+    var about: String
+    var grade: Int
+    var topicsOfInterest: [String]
+    var careerInterests: [String]
+    var mentorshipGoal: String?
+    var pairingId: String?
+    var status: String?
+    var mongoVersion: Int?
+    var whyPaired: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case name
+        case imageId
+        case about
+        case grade
+        case topicsOfInterest
+        case careerInterests
+        case mentorshipGoal
+        case pairingId
+        case status
+        case mongoVersion = "__v"
+        case whyPaired
+    }
 }
 
 class UserService {
