559a2b9a1ca7956fdb3184de05691df0200011bf
Aman Aggarwal <akaggarw@ucsd.edu>
2023-05-19 04:33:23 -0700

    Setup Admin Portal in nextJS with sidebar

---
+++ admin-portal/src/app/mentees/page.tsx
@@ -0,0 +1,10 @@
+export default function MenteesPage() {
+    return (
+      <>
+        <main>
+          <div>{'This is the mentees page!'}</div>
+        </main>
+      </>
+    );
+  }
+  
---
+++ admin-portal/src/icons/index.tsx
@@ -0,0 +1,6 @@
+export * from './SidebarMenteesIcon';
+export * from './SidebarMentorsIcon';
+export * from './SidebarSessionsIcon';
+export * from './ALUMLogoIcon'; 
+export * from './SidebarLogoutIcon';
+
