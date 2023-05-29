79d9ca2e3e35a78259051dd219d26eea16ae34ea
Aman Kumar Aggarwal <56745367+AmanKAggarwal@users.noreply.github.com>
2023-05-28 21:29:00 -0700

    Barebone setup for Admin Portal in nextJS (future of react) (#146)
    * Setup Admin Portal in nextJS with sidebar
    
    * Move authorized screens into its own directory so that their common layout is preserved even after login is added
    
    * update README about admin portal
    
    * add missing app/layout file
    
    * config file
    
    ---------
    
    Co-authored-by: christenx <99223047+christen03@users.noreply.github.com>

---
+++ admin-portal/src/icons/SidebarLogoutIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SidebarLogoutIcon = ({className}: {className?: string}) => {
+  return (
+    <svg className={className} width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M11.25 16.25H14.25C14.6478 16.25 15.0294 16.092 15.3107 15.8107C15.592 15.5294 15.75 15.1478 15.75 14.75V4.25C15.75 3.85218 15.592 3.47064 15.3107 3.18934C15.0294 2.90804 14.6478 2.75 14.25 2.75H11.25M6 13.25L2.25 9.5M2.25 9.5L6 5.75M2.25 9.5H11.25" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>    
+  );
+};
