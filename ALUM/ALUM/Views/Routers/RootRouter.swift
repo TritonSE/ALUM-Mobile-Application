76c11c5faed55bfc3b75a12a97c8524062287452
Philip Zhang <code@philipzha.ng>
2023-05-28 19:24:08 -0700

    implement network/internal error popup

--- ALUM/ALUM/Views/Routers/RootRouter.swift
+++ ALUM/ALUM/Views/Routers/RootRouter.swift
@@ -11,23 +11,72 @@ struct RootRouter: View {
     @ObservedObject var currentUser: CurrentUserModel = CurrentUserModel.shared
 
     var body: some View {
-        if self.currentUser.isLoading {
-            LoadingView(text: "RootView")
-            .onAppear(perform: {
-                Task {
-                    await self.currentUser.setForInSessionUser()
+        ZStack {
+            if self.currentUser.isLoading {
+                LoadingView(text: "RootView")
+                    .onAppear(perform: {
+                        Task {
+                            await self.currentUser.setForInSessionUser()
+                        }
+                    })
+            } else if self.currentUser.isLoggedIn == false {
+                NavigationView {
+                    LoginScreen()
                 }
-            })
-        } else if self.currentUser.isLoggedIn == false {
-            NavigationView {
-                LoginScreen()
+            } else if self.currentUser.status == "paired" {
+                CustomNavView {
+                    LoggedInRouter(defaultSelection: 0)
+                }
+            } else {
+                UnpairedScreen()
             }
-        } else if self.currentUser.status == "paired" {
-            CustomNavView {
-                LoggedInRouter(defaultSelection: 0)
+            errorMessage
+        }
+    }
+
+    var errorMessage: some View {
+        Group {
+            if self.currentUser.showInternalError || self.currentUser.showNetworkError {
+                GeometryReader { geometry in
+                    VStack {
+                        Spacer()
+                        VStack(spacing: 12) {
+                            if self.currentUser.showNetworkError {
+                                Image("NoConnectionIcon")
+                                Text("No internet connection")
+                                    .font(Font.custom("Metropolis-Regular", size: 17))
+                                Text("Please check your connection and try again.")
+                                    .font(.custom("Metropolis-Regular", size: 13))
+                                    .foregroundColor(Color("NeutralGray2"))
+                            }
+
+                            if self.currentUser.showInternalError {
+                                Image(systemName: "xmark.circle")
+                                    .font(.system(size: 50))
+                                    .foregroundColor(Color("ALUM Alert Red"))
+                                Text("Something went wrong")
+                                    .font(Font.custom("Metropolis-Regular", size: 17))
+                                Text("Please contact the ALUM team.")
+                                    .font(.custom("Metropolis-Regular", size: 13))
+                                    .foregroundColor(Color("NeutralGray2"))
+                            }
+
+                            Button("Dismiss") {
+                                self.currentUser.showInternalError = false
+                                self.currentUser.showNetworkError = false
+                            }
+                            .frame(minWidth: 50, maxWidth: 100)
+                            .frame(minHeight: 0, maxHeight: 48)
+                            .padding(.top, 20)
+                            .buttonStyle(FilledInButtonStyle(disabled: false))
+                        }
+                        .frame(width: geometry.size.width)
+                        .padding(.vertical, 36)
+                        .background(Rectangle().fill(Color.white).shadow(radius: 8))
+                    }
+                    .edgesIgnoringSafeArea(.bottom)
+                }
             }
-        } else {
-            UnpairedScreen()
         }
     }
 }
@@ -40,6 +89,7 @@ struct RootView_Previews: PreviewProvider {
             uid: "6431b9a2bcf4420fe9825fe5",
             role: .mentor
         )
+        CurrentUserModel.shared.showInternalError = true
         return RootRouter().onAppear(perform: {
             Task {
                 try await CurrentUserModel.shared.fetchUserInfoFromServer(
