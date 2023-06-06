b66a4b9bcb94cc159fb35a0cee0e41bbf8d8525c
christenx <99223047+christen03@users.noreply.github.com>
2023-06-05 21:53:59 -0700

    temp

--- admin-portal/src/app/page.tsx
+++ admin-portal/src/app/page.tsx
@@ -1,28 +1,21 @@
+"use client";
+
+import Image from 'next/image'
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { Login } from './login/page';
+import { useRouter } from 'next/navigation';
 
-const RootLayout = ({ children }) => {
-  // Your root layout content and structure goes here
-  return (
-    <div>
-      {/* Add any header, footer, or other common components */}
-      {children}
-    </div>
-  );
-};
+import styles from '@/styles/Page.module.css'
 
-export default function Home{  
-const router = useRouter();
+
+export default function Home() {
+  const router = useRouter();
 
   useEffect(() => {
-    router.push('/mentors');
-  }, []);
+    router.push('/login');
+  });
 
   return (
-    <RootLayout>
-      <Login />
-    </RootLayout>
+      null
   );
 };
 
