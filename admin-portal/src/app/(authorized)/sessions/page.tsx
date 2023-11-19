4a734e9c47c88df4e41aa37bc31e8d6345765bf6
adhi0331 <adhi03.ananthan@gmail.com>
2023-11-19 13:22:21 -0800

    added small lines

--- admin-portal/src/app/(authorized)/sessions/page.tsx
+++ admin-portal/src/app/(authorized)/sessions/page.tsx
@@ -2,7 +2,7 @@ export default function SessionsPage() {
   return (
     <>
       <main>
-        <div>{'This is the sessions page!'}</div>
+        <div>{'This is the sessions page! Here you will find all information regarding the sessions'}</div>
       </main>
     </>
   );
